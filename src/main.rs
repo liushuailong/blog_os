973c64f8010683f9dae4b8de53692feafbd108da
Liu Shuailong <liushuailong001@hotmail.com>
2023-12-13 21:21:37 +0800

    post 03

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,34 @@
 #![no_main] // 不使用rust提供的入口函数，main函数依赖运行时的实现，由于我们要自己编写操作系统，故需要使用硬件系统本身的入口函数
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+// static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // 在变异代码时，不对函数名称进行转化（保持名称不变）。
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    // vga_buffer::print_something();
+
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", Some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello world{}", "!");
+    println!("Hello world2{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
 // The standard library provides its own panic handler function, but in a no_std environment we need to define it ourselves:
 // 标准库中提供了panic处理函数，所以我们需要定义我们自己的panic处理函数
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> !{
+    println!("{}", info);
     loop {}
 }
