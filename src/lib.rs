8a43ea1190daff1cc3c67465d1a825f38bb542b6
Liu Shuailong <liushuailong001@hotmail.com>
2023-12-26 11:02:44 +0800

    post 09 section 1 code

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,22 @@ pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    init();
+    test_main();
+    hlt_loop();
+}
 
 pub trait Testable {
     fn run(&self) -> ();
--- src/main.rs
+++ src/main.rs
@@ -5,68 +5,118 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blog_os::println;
+use blog_os::{println, memory::translate_addr};
+use bootloader::{BootInfo, entry_point};
+use x86_64::structures::paging::{PageTable, Page};
+entry_point!(kernel_main);
 
 
-// static HELLO: &[u8] = b"Hello World!";
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory::active_level_4_table;
+    use x86_64::VirtAddr;
+    println!("Hello world {}", "!");
+    blog_os::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table: &'static mut PageTable = unsafe {
+    //     active_level_4_table(phys_mem_offset)
+    // };
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
 
-#[no_mangle] // 在变异代码时，不对函数名称进行转化（保持名称不变）。
-pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe { &*ptr };
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("L3 Entry {}: {:?}", i, entry);
+    //             }
+    //         }
     //     }
     // }
-    // vga_buffer::print_something();
 
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(vga_buffer::WRITER.lock(), ", Some numbers: {} {}", 42, 1.337).unwrap();
-    println!("Hello world{}", "!");
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
 
-    blog_os::init();
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset)};
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
-    // x86_64::instructions::interrupts::int3();
-
-    // unsafe {
-    //     *(0xdeadbedf as *mut u8) = 42;
-    // };
-
-    // fn stack_overflow() {
-    //     stack_overflow();
-    // }
-    // stack_overflow();
-
-    // let ptr = 0xdeadbeaf as *mut u8;
-    // unsafe { *ptr = 42; }
-
-    // let ptr = 0x2053ea as *mut u8;
-
-    // // read from a code page
-    // unsafe { let x = *ptr; }
-    // println!("read worked");
-
-    // // write to a code page
-    // unsafe { *ptr = 42; }
-    // println!("write worked");
-
-    use x86_64::registers::control::Cr3;
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
-
+    println!("It did not crash.");
     blog_os::hlt_loop();
-    // loop {
-    //     use blog_os::print;
-    //     print!("-");
-    // }
 }
+
+
+// static HELLO: &[u8] = b"Hello World!";
+
+// #[no_mangle] // 在变异代码时，不对函数名称进行转化（保持名称不变）。
+// pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+//     // let vga_buffer = 0xb8000 as *mut u8;
+//     // for (i, &byte) in HELLO.iter().enumerate() {
+//     //     unsafe {
+//     //         *vga_buffer.offset(i as isize * 2) = byte;
+//     //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+//     //     }
+//     // }
+//     // vga_buffer::print_something();
+
+//     // use core::fmt::Write;
+//     // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+//     // write!(vga_buffer::WRITER.lock(), ", Some numbers: {} {}", 42, 1.337).unwrap();
+//     println!("Hello world{}", "!");
+
+//     blog_os::init();
+
+//     // x86_64::instructions::interrupts::int3();
+
+//     // unsafe {
+//     //     *(0xdeadbedf as *mut u8) = 42;
+//     // };
+
+//     // fn stack_overflow() {
+//     //     stack_overflow();
+//     // }
+//     // stack_overflow();
+
+//     // let ptr = 0xdeadbeaf as *mut u8;
+//     // unsafe { *ptr = 42; }
+
+//     // let ptr = 0x2053ea as *mut u8;
+
+//     // // read from a code page
+//     // unsafe { let x = *ptr; }
+//     // println!("read worked");
+
+//     // // write to a code page
+//     // unsafe { *ptr = 42; }
+//     // println!("write worked");
+
+//     use x86_64::registers::control::Cr3;
+//     let (level_4_page_table, _) = Cr3::read();
+//     println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
+//     #[cfg(test)]
+//     test_main();
+
+//     println!("It did not crash!");
+
+//     blog_os::hlt_loop();
+//     // loop {
+//     //     use blog_os::print;
+//     //     print!("-");
+//     // }
+// }
 
 // The standard library provides its own panic handler function, but in a no_std environment we need to define it ourselves:
 // 标准库中提供了panic处理函数，所以我们需要定义我们自己的panic处理函数
