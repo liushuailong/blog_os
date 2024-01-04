f677b0828c1ab4e5c667e9b94efb9ed97c6ca2dc
Liu Shuailong <liushuailong001@hotmail.com>
2024-01-04 17:29:32 +0800

    post 12 comment

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -3,6 +3,7 @@ use crossbeam_queue::ArrayQueue;
 use crate::println;
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
+// 这个函数不应该在main.rs中调用，使用pub(crate)限制该函数只能在lib.rs库中使用;
 pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
@@ -42,6 +43,9 @@ impl Stream for ScanCodeSteam {
         if let Ok(scan_code) = queue.pop() {
             return Poll::Ready(Some(scan_code));
         }
+        // 为什么不是将waker注册后，直接返回Poll::Pending?
+        // 为什么要先注册后，再进行一次pop操作？
+        // 这是因为再注册后，可能立马就会唤醒，这是就会发生资源竞争，返回Poll::Ready;如果立马返回Poll::Rending，就会发生冲突；
         WAKER.register(&cx.waker());
         match queue.pop() {
             Ok(scan_code) => {
@@ -54,6 +58,8 @@ impl Stream for ScanCodeSteam {
 }
 
 use futures_util::task::AtomicWaker;
+// Waker 的注册、通知、删除，需要进一步理解，该过程不太清楚；
+// AtomicWaker 结构体实例化一个warker，向worker队列中注册任务的waker方法，使用waker的wake方法，唤醒waker中对应的任务，运行任务的；
 static WAKER: AtomicWaker = AtomicWaker::new();
 
 use futures_util::stream::StreamExt;
--- src/task/mod.rs
+++ src/task/mod.rs
@@ -41,6 +41,10 @@ use core::sync::atomic::{
 
 impl TaskId {
     fn new() -> Self {
+        // 怎么理解这个static变量？
+        // 通常理解taskid是递增的，每创建一个task，赋予一个taskid；
+        // 但是下面语句我理解为每创建一个taskid，这个静态变量NEXT_ID新生成初始化一次，这显然不符合实际；那推测自己在这个地方对这个静态变量的理解是错误的。
+        // 个人理解：在编译器 静态变量已经已经初始化，在程序运行的时候之后执行下面第二行的语句内容，第一行在编译期已经完成，打包在了二进制文件里面；因此这个初始化在函数内和函数外效果是一样的；
         static NEXT_ID: AtomicU64 = AtomicU64::new(0);
         TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
     }
